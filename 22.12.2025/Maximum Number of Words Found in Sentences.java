ab2e84a5f95a9604c998a391aaf91151c9799e4e
GopikaSri <gopikasri2k7@gmail.com>
2025-12-22 11:50:10 +0530

    Add method to find maximum words in sentences
    Implement a method to find the maximum number of words in sentences.

---
+++ 22.12.2025/Maximum Number of Words Found in Sentences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = Integer.MIN_VALUE;
+        for(String sent : sentences){
+            int count =0;
+            for(int i=0;i<sent.length();i++){
+                if(sent.charAt(i)==' '){
+                    count++;
+                }
+                if(count>max) max = count;
+            }
+
+        }
+        return max+1;
+    }
+}
