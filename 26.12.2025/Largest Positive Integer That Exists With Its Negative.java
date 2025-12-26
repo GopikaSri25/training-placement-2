11156bdb23ca0108b9932501200df820758f7fc9
GopikaSri <gopikasri2k7@gmail.com>
2025-12-26 11:46:12 +0530

    Update Largest Positive Integer That Exists With Its Negative.java

--- 26.12.2025/Largest Positive Integer That Exists With Its Negative.java
+++ 26.12.2025/Largest Positive Integer That Exists With Its Negative.java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+class Solution {
+    public int findMaxK(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        int max = -1;
+        for (int n : nums) {
+            if (n > 0 && set.contains(-n)) {
+                max = Math.max(max, n);
+            }
+        }
+        return max;
+    }
+}
