465e84dc065e3110fa406418f47e14eccc5dfd7d
GopikaSri <gopikasri2k7@gmail.com>
2025-12-28 14:38:35 +0530

    Create Row With Maximum Ones.py

---
+++ 28.12.2025/Row With Maximum Ones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rowAndMaximumOnes(self, mat):
+        row_index=0
+        max_cnt=0
+        for i in range(len(mat)):
+            cnt=Counter(mat[i])[1]
+            if cnt>max_cnt:
+                max_cnt=cnt
+                row_index=i
+        return [row_index,max_cnt]
