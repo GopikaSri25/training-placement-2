b9b7567fd5da510c011df3119e36706634626883
GopikaSri <gopikasri2k7@gmail.com>
2025-12-31 13:31:39 +0530

    Create Count Tested Devices After Test Operations.py

---
+++ 31.12.2025/Count Tested Devices After Test Operations.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def countTestedDevices(self, batteryPercentages):
+        tested=0
+        for battery in batteryPercentages:
+            if battery>tested:
+                tested+=1
+        return tested
