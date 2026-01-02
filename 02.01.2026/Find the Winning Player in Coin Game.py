91f3f6b83c8f9b5fa798894627dd4d82d86b18eb
GopikaSri <gopikasri2k7@gmail.com>
2026-01-02 13:34:05 +0530

    Create Find the Winning Player in Coin Game.py

---
+++ 02.01.2026/Find the Winning Player in Coin Game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def winningPlayer(self, x, y):
+        count = 0
+
+        while x>=1 and y >= 4:
+            x -= 1
+            y-=4
+            count += 1
+        if count % 2 == 0:
+            return "Bob"
+        return "Alice"
