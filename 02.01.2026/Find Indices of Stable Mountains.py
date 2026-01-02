95410a2da205374d4d8b42c05ef72e3c9b6089a2
GopikaSri <gopikasri2k7@gmail.com>
2026-01-02 14:25:29 +0530

    Create Find Indices of Stable Mountains.py

---
+++ 02.01.2026/Find Indices of Stable Mountains.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def stableMountains(self, height, threshold):
+        l=[]
+        for i in range(1,len(height)):
+            if(height[i-1]>threshold):
+                l.append(i)
+        return l
+        
