a5cd1f50d5ed935bc9df16e2921c4986f0a3ee55
GopikaSri <gopikasri2k7@gmail.com>
2026-01-02 12:54:24 +0530

    Create Find the Number of Good Pairs I.py

---
+++ 01.01.2026/Find the Number of Good Pairs I.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberOfPairs(self, nums1, nums2, k):
+        count = 0
+        for i in nums1:
+            for j in nums2:
+                if i % (j*k) == 0:
+                    count += 1
+        return count
+        
