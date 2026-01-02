4a8a805bd274044fd4c8eb9e53cf2beb6b29dc8b
GopikaSri <gopikasri2k7@gmail.com>
2026-01-02 12:19:35 +0530

    Create Latest Time You Can Obtain After Replacing Characters.py

---
+++ 01.01.2026/Latest Time You Can Obtain After Replacing Characters.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findLatestTime(self, s):
+        time = list(s)
+        if time[0] == '?' and time[1] == '?':
+            time[0]=time[1]='1'
+        elif time[0] == '?':
+            if time[1] <= '1':
+                time[0]='1'
+            else:
+                time[0]='0'
+        elif time[1]=='?':
+            if time[0] == '1':
+                time[1]='1'
+            else:
+                time[1]='9'
+        if time[3]=='?':
+            time[3]='5'
+        if time[4] == '?':
+            time[4]='9'
+        return "".join(time)
+        
+        
+        
