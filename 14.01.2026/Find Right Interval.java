a1e365ac83215c6081712ed0135352051ce879f7
GopikaSri <gopikasri2k7@gmail.com>
2026-01-14 17:51:40 +0530

    Create Find Right Interval.java

---
+++ 14.01.2026/Find Right Interval.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+       int n = intervals.length;
+        int[] result = new int[n];
+
+        int[][] starts = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            starts[i][0] = intervals[i][0];
+            starts[i][1] = i;              
+        }
+
+        Arrays.sort(starts, (a, b) -> a[0] - b[0]);
+
+        for (int i = 0; i < n; i++) {
+            int end = intervals[i][1];
+            int idx = binarySearch(starts, end);
+            result[i] = (idx == -1) ? -1 : starts[idx][1];
+        }
+
+        return result;
+ 
+    }
+
+    private static int binarySearch(int[][] starts, int target) {
+        int left = 0, right = starts.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (starts[mid][0] >= target) {
+                ans = mid;
+                right = mid - 1; 
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
